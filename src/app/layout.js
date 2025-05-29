6d61d46bb4013560094929d70c7373fa38d23f94
unknown <shaimazerin46@gmail.com>
2025-05-29 10:00:31 +0600

    font and color selected

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Questrial, Rubik } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const rubik = Rubik({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'], 
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const questrial = Questrial({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+  variable: '--font-questrial',
 });
 
 export const metadata = {
@@ -20,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       className={`${rubik.variable} ${questrial.variable}`}
       >
         {children}
       </body>
